60bf3e712ef125577153899c5afbe3d046240474
David Le Bansais <dlebansais@gmail.com>
2024-09-16 21:15:27 +0200

    Updated dependencies.

--- coverage.bat
+++ coverage.bat
@@ -13,6 +13,10 @@ set FRAMEWORK=net481
 set RESULTFILENAME=Coverage-%PROJECTNAME%.xml
 set RESULTFILEPATH=".\Test\%TESTPROJECTNAME%\bin\%PLATFORM%\%CONFIGURATION%\%FRAMEWORK%\%RESULTFILENAME%"
 
+set NUINT_CONSOLE_VERSION=3.15.5
+set NUINT_CONSOLE=NUnit.ConsoleRunner.%NUINT_CONSOLE_VERSION%
+set NUINT_CONSOLE_EXE=".\packages\%NUINT_CONSOLE%\tools\nunit3-console.exe"
+
 set OPENCOVER_VERSION=4.7.1221
 set OPENCOVER=OpenCover.%OPENCOVER_VERSION%
 set OPENCOVER_EXE=".\packages\%OPENCOVER%\tools\OpenCover.Console.exe"
@@ -21,21 +25,19 @@ set CODECOV_UPLOADER_VERSION=0.7.2
 set CODECOV_UPLOADER=CodecovUploader.%CODECOV_UPLOADER_VERSION%
 set CODECOV_UPLOADER_EXE=".\packages\%CODECOV_UPLOADER%\tools\codecov.exe"
 
-set NUINT_CONSOLE_VERSION=3.15.5
-set NUINT_CONSOLE=NUnit.ConsoleRunner.%NUINT_CONSOLE_VERSION%
-set NUINT_CONSOLE_EXE=".\packages\%NUINT_CONSOLE%\tools\nunit3-console.exe"
-
 set REPORTGENERATOR_VERSION=5.2.0
 set REPORTGENERATOR=ReportGenerator.%REPORTGENERATOR_VERSION%
 set REPORTGENERATOR_EXE=".\packages\%REPORTGENERATOR%\tools\net8.0\ReportGenerator.exe"
 
+nuget install NUnit.ConsoleRunner -Version %NUINT_CONSOLE_VERSION% -OutputDirectory packages
 nuget install OpenCover -Version %OPENCOVER_VERSION% -OutputDirectory packages
 nuget install CodecovUploader -Version %CODECOV_UPLOADER_VERSION% -OutputDirectory packages
-nuget install NUnit.ConsoleRunner -Version %NUINT_CONSOLE_VERSION% -OutputDirectory packages
+nuget install ReportGenerator -Version %REPORTGENERATOR_VERSION% -OutputDirectory packages
 
-if not exist %OPENCOVER_EXE% goto error_console1
-if not exist %CODECOV_UPLOADER_EXE% goto error_console2
-if not exist %NUINT_CONSOLE_EXE% goto error_console3
+if not exist %NUINT_CONSOLE_EXE% goto error_console1
+if not exist %OPENCOVER_EXE% goto error_console2
+if not exist %CODECOV_UPLOADER_EXE% goto error_console3
+if not exist %REPORTGENERATOR_EXE% goto error_console4
 
 if exist ".\Test\%TESTPROJECTNAME%\publish" rd /S /Q ".\Test\%TESTPROJECTNAME%\publish"
 
@@ -43,20 +45,29 @@ dotnet build ./Test/%TESTPROJECTNAME% /p:Platform=%PLATFORM% -c %CONFIGURATION% 
 
 if exist .\Test\%TESTPROJECTNAME%\*.log del .\Test\%TESTPROJECTNAME%\*.log
 if exist %RESULTFILEPATH% del %RESULTFILEPATH%
+
+rem Execute tests that will call OpenCover.
 dotnet test ./Test/%TESTPROJECTNAME% /p:Platform=%PLATFORM% -c %CONFIGURATION% -f %FRAMEWORK% --no-build --environment RESULTFILENAME="%RESULTFILENAME%"
-if exist %RESULTFILEPATH% %CODECOV_UPLOADER_EXE% -f %RESULTFILEPATH% -t %TOKEN%
+
+if not exist %RESULTFILEPATH% goto end
+%CODECOV_UPLOADER_EXE% -f %RESULTFILEPATH% -t %TOKEN%
+%REPORTGENERATOR_EXE% -reports:%RESULTFILEPATH% -targetdir:.\CoverageReports "-assemblyfilters:-Microsoft*;+%PROJECTNAME%" "-filefilters:-*.g.cs"
 goto end
 
 :error_console1
-echo ERROR: OpenCover.Console not found.
+echo ERROR: nunit3-console not found.
 goto end
 
 :error_console2
-echo ERROR: Codecov not found.
+echo ERROR: OpenCover.Console not found.
 goto end
 
 :error_console3
-echo ERROR: nunit3-console not found.
+echo ERROR: Codecov not found.
+goto end
+
+:error_console4
+echo ERROR: ReportGenerator not found.
 goto end
 
 :error_not_built
